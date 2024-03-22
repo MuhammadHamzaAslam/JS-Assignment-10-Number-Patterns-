60d8293005611aada54875e6096ab3aa74b084d4
MuhammadHamzaAslam <hamzaaslam19087@gmail.com>
2024-03-22 16:25:29 +0500

    commit

--- app.js
+++ app.js
@@ -59,21 +59,21 @@ for(var i = 7;i>=1;i--){
 document.write(`</section>`)
 
 
+document.write(`<section class="ab-1-1">`)
 for(var i = 7;i>=1;i--){
     for(var j = 1 ; j<=i ; j++){
         document.write(`${j}  `)
     }
     document.write(`<br>`)
 }
-// document.write(`<br>`)
-
-
+document.write(`<section class="ab-1-1">`)
 for(var i = 2;i<=7;i++){
     for(var j =1;j<=i;j++){
         document.write(`${j}  `)
     }
     document.write(`<br>`)
 }
+document.write(`</section>`)
 // document.write(`<br>`)
 // document.write(`<br>`)
 
@@ -169,3 +169,211 @@ for(var i = 6;i>=1;i--){
     }
     document.write(`<br>`)
 }
+
+
+
+document.write(`<h1>Star pattern</h1>`)
+
+
+//star patterns
+//star patterns
+//star patterns
+//star patterns
+//star patterns
+
+
+
+
+
+
+
+
+document.write(`<div>`)
+for(var i = 1;i<=7;i++){
+    for(var j =1;j<=i;j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`<br>`)
+
+for(var i = 7;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`<br>`)
+document.write(`</div>`)
+
+
+document.write(`<section>`)
+for(var i = 1;i<=7;i++){
+    for(var j = 1;j<=i;j++){
+        document.write(`* `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+document.write(`<br>`)
+
+document.write(`<section class="ab">`)
+
+for(var i = 7;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+
+
+document.write(`<section class="ab-1">`)
+for(var i = 1;i<=7;i++){
+    for(var j =1;j<=i;j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+document.write(`<br>`)
+
+document.write(`<section class="ab-1">`)
+for(var i = 7;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+
+document.write(`<section class="ab-1-1">`)
+for(var i = 7;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`<section class="ab-1-1">`)
+for(var i = 2;i<=7;i++){
+    for(var j =1;j<=i;j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+document.write(`<br>`)
+document.write(`<br>`)
+
+
+document.write(`<section class="ab-1">`)
+for(var i = 7;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`<section class="ab-1">`)
+for(var i = 2;i<=7;i++){
+    for(var j =1;j<=i;j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+document.write(`<section class="ab">`)
+
+for(var i = 7;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+document.write(`<section>`)
+for(var i = 2;i<=7;i++){
+    for(var j = 1;j<=i;j++){
+        document.write(`* `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+document.write(`<br>`)
+document.write(`<br>`)
+
+document.write(`<section>`)
+for(var i = 1;i<=7;i++){
+    for(var j = 1;j<=i;j++){
+        document.write(`* `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+document.write(`<section class="ab">`)
+
+for(var i = 6;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</section>`)
+
+document.write(`<br>`)
+
+document.write(`<header>`)
+for(var i = 1;i<=7;i++){
+    for(var j =1;j<=i;j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+for(var i = 6;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+document.write(`</header>`)
+
+document.write(`<br>`)
+
+for(var i = 1;i<=7;i++){
+    for(var j =1;j<=i;j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+
+for(var i = 6;i>=1;i--){
+    for(var j = 1 ; j<=i ; j++){
+        document.write(`*  `)
+    }
+    document.write(`<br>`)
+}
+
+
+// var arr = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+// for (var i = 0; i < arr.length; i++) {
+//     // console.log(arr[i]); 
+//     for(var j = 0; j <= i; j++){ 
+//         document.write(arr[i]);
+//     }
+//     document.write(`<br>`);
+// }
+
+
+
+
+
+
